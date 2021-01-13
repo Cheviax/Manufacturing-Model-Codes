e1f838f0d286f2361f6fb1949635dccf5286afe6
Marc <marc.duquesnoy@u-picardie.fr>
2021-01-13 10:43:14 +0100

    update 01/13

--- What Can Text Mining Tell Us About Lithium-Ion Battery Researchers’ Habits/BatteriesMining/BatteriesMining/__init__.py
+++ What Can Text Mining Tell Us About Lithium-Ion Battery Researchers’ Habits/BatteriesMining/BatteriesMining/__init__.py
@@ -4,6 +4,6 @@
 __version__ = "0.1.0"
 
 # from KeyWordsExtractions.Current_Density import *
-from BATWOMAN.Extracting_Informations import Converting_From_PDF_OR_XML_To_TXT,\
+from BatteriesMining.Extracting_Informations import Converting_From_PDF_OR_XML_To_TXT,\
     Filtring_And_Checking_Keywords
 # from KeyWordsExtractions.Filtring_Functions import *
